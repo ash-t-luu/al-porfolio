9d945269f45369819a0f13def50444e9a37e7f8f
ashleyluugit config --global user.email ashleyluu87@gmail.comgit config --global user.name ashleyluu <ashleyluu87@gmail.com>
2024-01-03 12:29:27 -0800

    changing path of json file to fix error in vercel d eployment

--- app/utils/action.tsx
+++ app/utils/action.tsx
@@ -1,7 +1,8 @@
 import { promises as fs } from 'fs';
 
 export default async function Data() {
-    const file = await fs.readFile(process.cwd() + '/public/projects.json', 'utf8');
+    const file = await fs.readFile(process.cwd() + '/app/projects.json', 'utf8');
     const data = JSON.parse(file);
+    console.log('data in action: ', data);
     return data;
 }
